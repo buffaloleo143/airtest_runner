c97b6a74f9bf6617ba7b15d04a1e521b25720a40
buffaloleo143 <46956140+buffaloleo143@users.noreply.github.com>
2019-08-23 10:59:24 +0800

    Add files via upload

---
+++ scripts/test.air/test.py
@@ -0,0 +1,12 @@
+# -*- encoding=utf8 -*-
+__author__ = "Leo Zeng"
+
+from airtest.core.api import *
+
+auto_setup(__file__)
+
+snapshot(msg="请填写测试点.")
+sleep(5)
+snapshot(msg="请填写测试点.")
+sleep(5)
+snapshot(msg="请填写测试点.")
